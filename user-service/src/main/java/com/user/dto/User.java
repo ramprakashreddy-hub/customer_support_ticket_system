216af5c1498020fdc4f09620f35805ec9be2dab7
Ramprakash Reddy <ramprakashreddy069@gmail.com>
2025-07-29 23:57:30 +0530

    feat: add user service

---
+++ user-service/src/main/java/com/user/dto/User.java
@@ -0,0 +1,27 @@
+package com.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String userId;
+    private String name;
+    private String email;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String password;
+    private String role;
+    
+    public User(String userId, String name, String email, String role) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+}
